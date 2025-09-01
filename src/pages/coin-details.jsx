e751f814318116e618e95bbe1de249e5493b56d2
saulopm <saulopmarinho@terra.com.br>
2025-08-31 21:46:53 -0300

    first commit

---
+++ src/pages/coin-details.jsx
@@ -0,0 +1,71 @@
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router";
+const API_URL = import.meta.env.VITE_COIN
+
+const CoinDetailsPage = () => {
+
+    const { id } = useParams()
+    const [coin, setCoin] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => { 
+        const fetchCoin = async () => {
+            try {
+                const res = await fetch(`${API_URL}/${id}`)
+                if(!res.ok) throw new Error('Failed to fetch the data')
+                const data = await res.json()
+
+            setCoin(data)
+            } catch(err) {
+                console.log(err)
+                setError(err.message)
+            } finally {
+                setLoading(false)
+            }            
+        }
+
+        fetchCoin()
+    }, [id])
+    
+
+    return  <div className="coin-details-container">
+        <Link to='/'> ← Back to Home</Link>
+
+        <h1 className="coin-details-title">
+            { coin ? `${coin.name} (${coin.symbol})` : 'Coin Details'}
+        </h1>
+
+        {loading && <p>Loading ....</p>}
+        {error && <div className="error"> ❌ {error}</div>}
+
+        {!loading && !error && (
+            <>
+            <img src={coin.image.large} alt={coin.name}
+             className="coins-details-image"
+              />
+
+              <p>{coin.description.en.split('. ')[0] + '.'}</p>
+
+              <div className="coin-details-info">
+                <h3>Rank: {coin.market_cap_rank}</h3>
+                <h3>Current Price : $ { coin.market_data_current_price.usd.toLocaleString()}</h3>
+                <h4>Market Cap : $ {coin.market_data.market_cap.usd.toLocaleString()}</h4>
+                <h4>24h High : $ {coin.market_data.high_24h.usd.toLocaleString()}</h4>
+                <h4>24h Low : $ {coin.market_data.low_24h.usd.toLocaleString()}</h4>
+                <h4>
+              24h Price Change: ${coin.market_data.price_change_24h.toFixed(2)}{' '}
+              ({coin.market_data.price_change_percentage_24h.toFixed(2)}%)
+            </h4>
+            <h4>
+              Circulating Supply:{' '}
+              {coin.market_data.circulating_supply.toLocaleString()}
+            </h4>
+                </div>
+            </>
+        )} 
+           
+    </div> ;
+}
+ 
+export default CoinDetailsPage;
